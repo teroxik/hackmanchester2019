837ffa1858ad40f0a34995fc8fca47f72fce3a44
Michal Janousek <janousek.michal@gmail.com>
2019-10-26 19:07:46 +0100

    Test

--- functions/src/clockwork/index.js
+++ functions/src/clockwork/index.js
@@ -1,3 +1,4 @@
+import * as admin from 'firebase-admin'
 import * as functions from 'firebase-functions'
 
 /**
@@ -8,10 +9,54 @@ import * as functions from 'firebase-functions'
  */
 export async function clockworkRequest(req, res) {
   // Write response to request to end function execution
-  console.log(JSON.stringify(req.query))
-  console.log('The data')
+  console.log(`The sms data -  ${JSON.stringify(req.query)}`)
+
+  await handleText(req.query)
+
   res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
-  res.end('Hello from clockwork')
+  res.end('Sms process')
+}
+
+const handleText = async message => {
+  const number = message.from
+  const content = message.content
+  if (content && content.length && content.length > 1) {
+    const firstChar = content[0]
+    const secondChar = content[1]
+    switch (firstChar) {
+      case 'V':
+        await hasVoted(secondChar, number)
+        break
+      case 'R':
+        await takenRightPod(secondChar, number)
+        break
+      case 'W':
+        await takenWrongPod(secondChar, number)
+        break
+      default:
+        console.log('wrong message idiot')
+    }
+  } else {
+    console.log('content is empty idiot')
+  }
+}
+
+const takenRightPod = async (podNumber, number) => {
+  const digest = { pod: podNumber, correctPod: true, timestamp: new Date() }
+  const db = admin.firestore()
+  db.collection(`/patients/${number}/digests`).add(digest)
+}
+
+const takenWrongPod = async (podNumber, number) => {
+  const digest = { pod: podNumber, correctPod: false, timestamp: new Date() }
+  const db = admin.firestore()
+  db.collection(`/patients/${number}/digests`).add(digest)
+}
+
+const hasVoted = async (voteNumber, number) => {
+  const vote = { vote: voteNumber, correctPod: false, timestamp: new Date() }
+  const db = admin.firestore()
+  db.collection(`/patients/${number}/votes`).add(vote)
 }
 
 /**
--- functions/src/scheduler/index.js
+++ functions/src/scheduler/index.js
@@ -1,14 +1,17 @@
 import * as functions from 'firebase-functions'
+import got from 'got'
 
 /**
- * Handle scheduler pub sub event
- * @param {functions.pubsub.Context} context - Function context
- * @returns {Promise}
+ * Cloud Function triggered on a specified CRON schedule
+ *
+ * Trigger: `PubSub - onRun`
+ * @name scheduler
+ * @type {functions.CloudFunction}
  */
 async function schedulerEvent(context) {
   console.log('Pub Sub message: ', { context })
-
   // End function execution by returning
+  got('https://us-central1-hackmanchester2019.cloudfunctions.net/processor')
   return null
 }
 
