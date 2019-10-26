2982b3148dcd4de661bad31d569abd634200ebc8
Michal Janousek <janousek.michal@gmail.com>
2019-10-26 14:47:34 +0100

    Changes of the config

--- functions/src/clockwork/index.js
+++ functions/src/clockwork/index.js
@@ -1,16 +1,16 @@
 import * as functions from 'firebase-functions'
 
 /**
-  * Handle request from calling /clockwork endpoint
-  * @param {object} req - Express HTTP Request
-  * @param {object} res - Express HTTP Response
-  * @returns {Promise} Resolves after handling request
-  */
- export async function clockworkRequest(req, res) {
-   // Write response to request to end function execution
-   res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
-   res.end('Hello from clockwork')
- }
+ * Handle request from calling /clockwork endpoint
+ * @param {object} req - Express HTTP Request
+ * @param {object} res - Express HTTP Response
+ * @returns {Promise} Resolves after handling request
+ */
+export async function clockworkRequest(req, res) {
+  // Write response to request to end function execution
+  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
+  res.end('Hello from clockwork')
+}
 
 /**
  * @name clockwork
